5fcba69bbcece933183ca7ef18a7ee1ab5e2f2f0
kzakaryan <kzakaryan@griddynamics.com>
2025-03-23 15:33:16 +0400

    Completing Task 2

--- capstone-data/src/main/java/am/capstone/project/Instructor.java
+++ capstone-data/src/main/java/am/capstone/project/Instructor.java
@@ -1,12 +1,10 @@
 package am.capstone.project;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import java.util.Set;
 
-@Getter
-@Setter
+@Data
 @Entity
 public class Instructor {
 
