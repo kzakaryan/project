a7e27b459bf3a4174affc7d2de223071cb89314a
kzakaryan <kzakaryan@griddynamics.com>
2025-03-31 11:08:28 +0400

    Adding Lits of Admins and Courses

--- capstone-web/src/main/java/am/capstone/project/controller/AdministratorController.java
+++ capstone-web/src/main/java/am/capstone/project/controller/AdministratorController.java
@@ -1,15 +1,25 @@
 package am.capstone.project.controller;
 
 
+import am.capstone.project.service.AdministratorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/administrators")
 @Controller
 public class AdministratorController {
 
+    private final AdministratorService administratorService;
+
+    public AdministratorController(AdministratorService administratorService) {
+        this.administratorService = administratorService;
+    }
+
+
     @RequestMapping({"/", "/index", "/index.html"})
-    public String administratorsIndex() {
+    public String administratorsIndex(Model model) {
+        model.addAttribute("administrators", administratorService.findAll());
         return "administrators/index";
     }
 }
