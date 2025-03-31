47d68deafcd1a30bda554f5528b6495ae8a82acf
kzakaryan <kzakaryan@griddynamics.com>
2025-03-31 11:18:02 +0400

    Adding index and controller for every model class

--- capstone-web/src/main/java/am/capstone/project/controller/InstructorController.java
+++ capstone-web/src/main/java/am/capstone/project/controller/InstructorController.java
@@ -1,14 +1,23 @@
 package am.capstone.project.controller;
 
+import am.capstone.project.service.InstructorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/instructors")
 @Controller
 public class InstructorController {
 
+    private final InstructorService instructorService;
+
+    public InstructorController(InstructorService instructorService) {
+        this.instructorService = instructorService;
+    }
+
     @RequestMapping({"/", "/index", "/index.html"})
-    public String instructorsIndex() {
+    public String instructorsIndex(Model model) {
+        model.addAttribute("instructors", instructorService.findAll());
         return "instructors/index";
     }
 }
--- capstone-web/src/main/java/am/capstone/project/controller/StudentController.java
+++ capstone-web/src/main/java/am/capstone/project/controller/StudentController.java
@@ -1,14 +1,23 @@
 package am.capstone.project.controller;
 
+import am.capstone.project.service.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/students")
 @Controller
 public class StudentController {
 
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @RequestMapping({"/", "/index", "/index.html"})
-    public String studentsIndex() {
+    public String studentsIndex(Model model) {
+        model.addAttribute("students", studentService.findAll());
         return "students/index";
     }
 }
